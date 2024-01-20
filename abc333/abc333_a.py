3220ce8df341f8adf226dc82d1dd4864db229ae7
kaigitt <s12201600350@gmail.com>
2024-01-20 14:52:12 +0000

    Added my codes from AtCoder

---
+++ abc333/abc333_a.py
@@ -0,0 +1,16 @@
+import math
+
+
+#N, S = map(int, input().split())
+
+#N = int(input())
+#A = list(map(int, input().split()))
+#P = list(map(int, input().split()))
+
+A = int(input())
+
+res = ""
+for i in range(A):
+    res += str(A)
+
+print(res)
